2499887c9b8e8ddd6e4005378837c14fc8df123a
ZackWong5505 <kimkit1996@hotmail.com>
2024-07-22 00:58:06 +0800

    add new api and schduler

--- customer/src/main/java/com/customer/service/CustomerService.java
+++ customer/src/main/java/com/customer/service/CustomerService.java
@@ -4,12 +4,13 @@ import com.customer.modal.CustomerDTO;
 import com.customer.request.AddCustomerRequest;
 import com.customer.request.DeleteCustomerRequest;
 import com.customer.request.UpdateCustomerRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 public interface CustomerService {
     public CustomerDTO getCustomerDetail(String name);
     public void doAddCustomer (AddCustomerRequest addCustomerRequest);
     public void updateCustomer(UpdateCustomerRequest updateCustomerRequest);
     public void deleteCustomer(DeleteCustomerRequest deleteCustomerRequest);
-
+    public String getExternalData() throws JsonProcessingException;
 
 }
